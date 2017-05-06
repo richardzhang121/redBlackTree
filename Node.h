9dedc4b3ca6249125c406eaac407d113a36bff5e
richardzhang121 <richard.zhang121@gmail.com>
2017-05-05 20:28:25 -0700

    Create Node.h

---
+++ Node.h
@@ -0,0 +1,34 @@
+#ifndef NODE_H
+#define NODE_H
+
+using namespace std;
+
+
+class Node{
+ public:
+  Node(int newData);
+  Node();
+  ~Node();
+  Node* getParent();
+  Node* getLeft(); //returns the next node
+  Node* getRight();
+  Node* getUncle();
+  int getData(); //returns the student the node points to
+  void setLeft(Node* node); //sets the pointer to the next node
+  void setRight(Node* node);
+  void setParent(Node* node);
+  bool isRed();
+  void setRed(bool isRed);
+  bool isRight();
+  void setIsRight(bool isRight);
+ protected:
+  int data;
+  Node* left;
+  Node* right;
+  Node* parent;
+  bool red;
+  bool rightChild;
+};
+
+
+#endif
