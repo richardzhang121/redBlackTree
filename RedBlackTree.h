6f385a3743383c448d5d3168e0abc5285f7cd355
richardzhang121 <richard.zhang121@gmail.com>
2017-05-18 13:25:54 -0700

    Update RedBlackTree.h

--- RedBlackTree.h
+++ RedBlackTree.h
@@ -6,11 +6,17 @@ using namespace std;
 class RedBlackTree{
  private:
   Node* head;
+  void rotateLeft(Node* current);
+  void rotateRight(Node* current);
+  void fix(Node* current);
+  void fix2Black(Node* black, Node* parent);
 
  public:
   RedBlackTree();
   void add(Node* current, int number);  
   void print(Node* current, int indent = 0);
   Node* getHead();
+  Node* search(int number, Node* current = NULL);
+  void remove(Node* toDelete);
 
 };
